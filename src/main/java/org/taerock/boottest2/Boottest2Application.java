713107df61f3756caf225a867bbf3b5cc7b9c364
LTH4823 <peter4823zx@naver.com>
2022-06-30 22:36:29 +0900

    Initial commit

---
+++ src/main/java/org/taerock/boottest2/Boottest2Application.java
@@ -0,0 +1,13 @@
+package org.taerock.boottest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boottest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boottest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/taerock/boottest2/Boottest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taerock.boottest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boottest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
